bb87fca9059b710c4676d94bba78148049acb4dc
Mariele Ribeiro <mariele_rbr@hotmail.com>
2021-05-11 13:18:19 -0300

    Memo

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,27 @@ interface ProductItemProps {
   };
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
+
+// Memo -> vai conseguir evitar que o react crie uma nova versão do componente se nenhuma propriedade do componente tenha sido alterada
+// Ele faz uma shallow compare(comparação rasa) => ele basicamente verifica a igualdade das informações que eu tenho
+// dentro das propriedades
+
+// Em quais situaçoes usar o memo =>
+// 1. Pure Functional(dados os mesmo parametros sempre retornam o mesmo resultado) Components(componentes puros)= componentes apenas para abstrair alguma parte visual da aplicação
+// 2. Render too often(componentes que renderizam damais)
+// 3. Re-renders with same props(componente renderiza novamente com as mesmas props)
+// 4. Medium to big size
